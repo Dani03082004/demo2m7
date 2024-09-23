dd610fc2aabdf50118672334a8ad773c6d0f4e38
danibaneza25@gmail.com <danibaneza25@gmail.com>
2024-09-23 19:30:00 +0200

    He canviado el index

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
     
     require 'config.php';
-    require CONTR.'add.php';
-    var_dump($routes);
+    function router ($url,$routes){
+        if(in_array($url,$routes)){
+            return $url;
+    }
+    return "home";
+}
+$url=$_SERVER['REQUEST_URI'];
+$path=parse_url($url)['path'];
+//$query=parse_url($path)['query'];
+$path=explode("/",$path);
+$uri=array_filter($path,function($item){
+    if($item!=""){
+        return $item;
+    }
+});
+$uri=array_values($uri);
+$contr=router($uri[0],routes: $routes);
+var_dump(value: $contr);
+
+
+    // require CONTR.'/add.php';
+    //var_dump($path,$query);
     //front controller
   //  require VIEWS.'/index.view.php';
